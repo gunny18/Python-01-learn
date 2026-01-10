d636410a1593e9255ced70c145e6fbacd8e27203
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2026-01-10 11:37:08 +0530

    Added integers and floats

---
+++ integers_and_floats.py
@@ -0,0 +1,28 @@
+# int
+num1 = 1
+
+# float
+num2 = 3.45
+
+# python type
+print(type(num1))
+print(type(num2))
+
+num3 = '100'
+num4 = '200'
+print(num3 + num4)
+
+# type coercion
+num3 = int(num3)
+num4 = int(num4)
+print(num3 + num4)
+
+# Absolute value
+print(abs(-33))
+
+# rounded value
+print(round(num2))
+print(round(num2,1))
+
+# Operators -> +, -, *, /, //, **, %
+# Comparisons -> ==, !=, <, >, <=, >=
