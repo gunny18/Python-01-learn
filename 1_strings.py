229a0d82c14e5ed4483ff8720b7a230b22cf3bce
Ganeshswaminathan R <ganeshswaminathanr@Ganeshswaminathans-MacBook-Air.local>
2026-06-22 15:42:18 +0530

    Added comments for string

--- 1_strings.py
+++ 1_strings.py
@@ -1,12 +1,18 @@
+# Double quotes to write string
 message = "Hello"
 
+# In case single quote is part of string
 message_1 = "Hello'qqq"
 
+# Use of single quote string and if " is part of string
 message_2 = 'Hello"we'
 
+# Multiline string using double quote
 message_3 = """Hello, My name is
 Gunny."""
 
+
+# Multiline string using single quote
 message_4 = '''
 Hi
 I
@@ -32,17 +38,22 @@ message_6 = "{}, {}".format(message, message_1)
 # f-string
 message_7 = f"{message} asdasd {message_1}."
 
-# Slicing
+# Slicing - inclusive of start index, exclusive of end index
 print(message_1[1:3])
 print(message_1[:5])
 print(message_1[3:6])
 print(message_1[3:])
 print(message[:])
 
-# Methods
+# Methods on string data.
+
+# Returns new string, does not modify orginal string
 print(message.lower())
 print(message.upper())
-print(message.count("e"))
-print(message.find("lo"))
 replaced_message = message.replace("Hel", "WTFFFF")
 
+# Returns occureces of string/sequence
+print(message.count("e"))
+
+# Returns index of first sub-string if found.
+print(message.find("lo"))
