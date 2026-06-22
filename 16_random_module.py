4a7b4d3dfa2c49cb8c23373590252dc04b80a26f
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2026-06-22 17:19:40 +0530

    Added random module sampling comments

--- 16_random_module.py
+++ 16_random_module.py
@@ -15,10 +15,12 @@ print(val)
 
 
 colors = ["Red", "Black", "Green"]
+# choices selects elements with replacement i.e. elements can appear again (Sampling with replacement)
 val = random.choices(colors, k=10)
 print(val)
 
-
+# It means final list will have 45% red, 45% black and 10% green
+# More weight = More chances of that element getting selected
 val = random.choices(colors, k=10, weights=[45, 45, 10])
 print(val)
 
@@ -28,5 +30,6 @@ print(deck)
 random.shuffle(deck)
 print(deck)
 
+# sample selcets elements without replacement, i.e. same element cannot appear again (Sampling without replacement)
 hand = random.sample(deck, k=5)
 print(hand)
